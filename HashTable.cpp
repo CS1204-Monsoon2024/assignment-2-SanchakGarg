268af90ddf26a5ebaa66c982aa5fe974bade50de
SanchakGarg <sanchakgargss@gmail.com>
2024-10-19 11:47:41 +0530

    Hashtable test 1

---
+++ HashTable.cpp
@@ -0,0 +1,129 @@
+#include <iostream>
+
+class HashTable {
+private:
+    int* table;
+    int size;
+    int count;
+    const int EMPTY = -1;
+    const int DELETED = -2;
+
+    int nextPrime(int n) {
+        while (!isPrime(n)) {
+            ++n;
+        }
+        return n;
+    }
+
+    bool isPrime(int n) {
+        if (n < 2) return false;
+        for (int i = 2; i * i <= n; ++i) {
+            if (n % i == 0) return false;
+        }
+        return true;
+    }
+
+    int hash(int key) {
+        return key % size;
+    }
+
+    int quadraticProbing(int key, int i) {
+        return (hash(key) + i * i) % size;
+    }
+
+    void resize() {
+        int oldSize = size;
+        size = nextPrime(size * 2);
+        int* oldTable = table;
+        table = new int[size];
+        for (int i = 0; i < size; ++i) table[i] = EMPTY;
+        count = 0;
+
+        for (int i = 0; i < oldSize; ++i) {
+            if (oldTable[i] != EMPTY && oldTable[i] != DELETED) {
+                insert(oldTable[i]);
+            }
+        }
+        delete[] oldTable;
+    }
+
+    double loadFactor() {
+        return (double)count / size;
+    }
+
+public:
+    HashTable(int initialSize) {
+        size = nextPrime(initialSize);
+        table = new int[size];
+        for (int i = 0; i < size; ++i) table[i] = EMPTY;
+        count = 0;
+    }
+
+    ~HashTable() {
+        delete[] table;
+    }
+
+    void insert(int key) {
+        if (search(key) != -1) {
+            std::cout << "Duplicate key insertion is not allowed" << std::endl;
+            return;
+        }
+
+        if (loadFactor() > 0.8) {
+            resize();
+        }
+
+        int i = 0;
+        int index;
+        while (i < size) {
+            index = quadraticProbing(key, i);
+            if (table[index] == EMPTY || table[index] == DELETED) {
+                table[index] = key;
+                count++;
+                return;
+            }
+            i++;
+        }
+
+        std::cout << "Max probing limit reached!" << std::endl;
+    }
+
+    int search(int key) {
+        int i = 0;
+        int index;
+        while (i < size) {
+            index = quadraticProbing(key, i);
+            if (table[index] == EMPTY) {
+                return -1;
+            }
+            if (table[index] == key) {
+                return index;
+            }
+            i++;
+        }
+        return -1;
+    }
+
+    void remove(int key) {
+        int index = search(key);
+        if (index == -1) {
+            std::cout << "Element not found" << std::endl;
+        } else {
+            table[index] = DELETED;
+            count--;
+        }
+    }
+
+    void printTable() {
+        for (int i = 0; i < size; ++i) {
+            if (table[i] == EMPTY) {
+                std::cout << "- ";
+            } else if (table[i] == DELETED) {
+                std::cout << "- ";
+            } else {
+                std::cout << table[i] << " ";
+            }
+        }
+        std::cout << std::endl;
+    }
+};
